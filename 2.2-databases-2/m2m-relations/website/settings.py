ece5be1207b416512aaeaac3761e609d66182d7f
WEGASS <rden2000@gmail.com>
2022-09-10 17:55:51 +0300

    Done homework 2.2

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'Kras1n1jLed',
+        'PASSWORD': 'postgres',
     }
 }
 
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'Kras1n1jLed',
+        'PASSWORD': 'postgres',
     }
 }
 
