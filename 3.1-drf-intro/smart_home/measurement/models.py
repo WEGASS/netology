3bdf5991b9c6c1779384a8327769a4fc2551946f
WEGASS <rden2000@gmail.com>
2022-09-23 02:44:49 +0300

    Done homework 3.1

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor,
+                               on_delete=models.CASCADE,
+                               related_name='measurements',
+                               verbose_name='Датчик')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True, verbose_name='Изображение')
+
+    def __str__(self):
+        return f'{self.sensor} - {self.created_at}'
